32e240b2438190695a22848275424b45ae1d4306
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-11-24 16:22:41 +0300

    save formData to localStroage

--- components/AddNewItem.jsx
+++ components/AddNewItem.jsx
@@ -1,82 +1,123 @@
-import React from 'react'
+import React, { useState } from "react";
+import { addTransaction } from "@/redux/transactionsSlice";
+import { useDispatch } from "react-redux";
 
 const AddNewItem = () => {
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    category: "Gelir",
+    description: "",
+    amount: "",
+    date: "",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Redux'a veri gönder
+    dispatch(addTransaction(formData));
+
+    // Formu sıfırla
+    setFormData({
+      category: "Gelir",
+      description: "",
+      amount: "",
+      date: "",
+    });
+  };
+
   return (
     <div className="bg-white shadow-lg rounded-lg p-6">
-              <h3 className="text-xl font-medium text-gray-700 mb-4">
-                Gelir/Gider Ekle
-              </h3>
-              <form>
-                {/* Kategori Seçimi */}
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-medium text-gray-700 mb-2"
-                    htmlFor="category"
-                  >
-                    Kategori
-                  </label>
-                  <select
-                    id="category"
-                    className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  >
-                    <option>Gelir</option>
-                    <option>Gider</option>
-                  </select>
-                </div>
-                {/* Açıklama */}
-                <div className="mb-4">
-                  <label
-                    htmlFor="description"
-                    className="block text-sm font-medium text-gray-600 mb-2"
-                  >
-                    Açıklama
-                  </label>
-                  <input
-                    type="text"
-                    id="description"
-                    placeholder="Açıklama girin"
-                    className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
-                  />
-                </div>
-                {/* Tutar */}
-                <div className="mb-4">
-                  <label
-                    htmlFor="amount"
-                    className="block text-sm font-medium text-gray-600 mb-2"
-                  >
-                    Tutar
-                  </label>
-                  <input
-                    type="number"
-                    id="amount"
-                    placeholder="₺"
-                    className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-400"
-                  />
-                </div>
-                {/* Tarih */}
-                <div className="mb-4">
-                  <label
-                    htmlFor="date"
-                    className="block text-sm font-medium text-gray-600 mb-2"
-                  >
-                    Tarih
-                  </label>
-                  <input
-                    type="date"
-                    id="date"
-                    className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                  />
-                </div>
-                {/* Gönder Butonu */}
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-white font-semibold px-6 py-3 rounded-lg shadow hover:bg-blue-600 w-full"
-                >
-                  Kaydet
-                </button>
-              </form>
-            </div>
-  )
-}
+      <h3 className="text-xl font-medium text-gray-700 mb-4">
+        Gelir/Gider Ekle
+      </h3>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            className="block text-sm font-medium text-gray-700 mb-2"
+            htmlFor="category"
+          >
+            Kategori
+          </label>
+          <select
+            id="category"
+            value={formData.category}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            <option>Gelir</option>
+            <option>Gider</option>
+          </select>
+        </div>
 
-export default AddNewItem
+        <div className="mb-4">
+          <label
+            htmlFor="description"
+            className="block text-sm font-medium text-gray-600 mb-2"
+          >
+            Açıklama
+          </label>
+          <input
+            type="text"
+            id="description"
+            value={formData.description}
+            onChange={handleChange}
+            placeholder="Açıklama girin"
+            className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="amount"
+            className="block text-sm font-medium text-gray-600 mb-2"
+          >
+            Tutar
+          </label>
+          <input
+            type="number"
+            id="amount"
+            value={formData.amount}
+            onChange={handleChange}
+            placeholder="₺"
+            className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-400"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="date"
+            className="block text-sm font-medium text-gray-600 mb-2"
+          >
+            Tarih
+          </label>
+          <input
+            type="date"
+            id="date"
+            value={formData.date}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white font-semibold px-6 py-3 rounded-lg shadow hover:bg-blue-600 w-full"
+        >
+          Kaydet
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddNewItem;
